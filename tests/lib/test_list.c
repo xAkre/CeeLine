8949621df25038f032975c88ae59b9dee8614be7
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-11-08 23:22:08 +0100

    Create tests for ll_insert_before_node function

--- tests/lib/test_list.c
+++ tests/lib/test_list.c
@@ -113,6 +113,58 @@ void test_ll_pop()
     return;
 }
 
+void test_ll_insert_before_node()
+{
+    printf("Testing ll_insert_before_node\n");
+
+    struct LinkedList *linked_list = ll_create(int_compare_function, int_free_function);
+
+    int *value = malloc(sizeof(int));
+    *value = 42;
+    int *value2 = malloc(sizeof(int));
+    *value2 = 43;
+    int *value3 = malloc(sizeof(int));
+    *value3 = 44;
+
+    ll_push(linked_list, value);
+
+    struct LinkedListNode *node = linked_list->head;
+
+    assert(ll_insert_before_node(linked_list, node, value2) == 0);
+    assert(linked_list->size == 2);
+    assert(linked_list->head != NULL);
+    assert(linked_list->tail != NULL);
+    assert(linked_list->head != linked_list->tail);
+    assert(linked_list->head->value == value2);
+    assert(linked_list->head->next == linked_list->tail);
+    assert(linked_list->tail->value == value);
+    assert(linked_list->tail->next == NULL);
+
+    assert(ll_insert_before_node(linked_list, linked_list->tail, value3) == 0);
+    assert(linked_list->size == 3);
+    assert(linked_list->head != NULL);
+    assert(linked_list->tail != NULL);
+    assert(linked_list->head != linked_list->tail);
+    assert(linked_list->head->value == value2);
+    assert(linked_list->head->next != linked_list->tail);
+    assert(linked_list->head->next->value == value3);
+    assert(linked_list->head->next->next == linked_list->tail);
+    assert(linked_list->tail->value == value);
+    assert(linked_list->tail->next == NULL);
+
+    struct LinkedListNode *non_existent_node = malloc(sizeof(struct LinkedListNode));
+    non_existent_node->value = value;
+    non_existent_node->next = NULL;
+
+    assert(ll_insert_before_node(linked_list, non_existent_node, value3) == -1);
+
+    ll_free(linked_list);
+
+    printf("ll_insert_before_node passed\n");
+
+    return;
+}
+
 int main()
 {
     printf("Running tests for \"lib/list.c\"\n");
@@ -120,6 +172,7 @@ int main()
     test_ll_create();
     test_ll_push();
     test_ll_pop();
+    test_ll_insert_before_node();
 
     printf("All tests passed for \"lib/list.c\"\n");
 
