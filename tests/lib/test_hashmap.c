a03a16eefea7383991bcb7ec672284811824094e
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-11-09 14:21:29 +0100

    Create tests for hm_set function

--- tests/lib/test_hashmap.c
+++ tests/lib/test_hashmap.c
@@ -41,13 +41,82 @@ void test_hm_create()
     return;
 }
 
+void test_hm_set()
+{
+    printf("Testing hm_set\n");
+
+    struct HashMap *hashmap = hm_create(10, int_hash_function, int_compare_function,
+                                        int_free_function, int_free_function);
+
+    int *key = malloc(sizeof(int));
+    *key = 0;
+    int *value = malloc(sizeof(int));
+    *value = 10;
+
+    assert(hm_set(hashmap, key, value) == 0);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->value)->key ==
+           key);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->value)->value ==
+           value);
+
+    int *key2 = malloc(sizeof(int));
+    *key2 = 10;
+    int *value2 = malloc(sizeof(int));
+    *value2 = 20;
+
+    assert(hm_set(hashmap, key2, value2) == 0);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->value)->key ==
+           key);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->value)->value ==
+           value);
+    assert(
+        ((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->next->value)->key ==
+        key2);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->next->value)
+               ->value == value2);
+
+    int *key3 = malloc(sizeof(int));
+    *key3 = 1;
+    int *value3 = malloc(sizeof(int));
+    *value3 = 30;
+
+    assert(hm_set(hashmap, key3, value3) == 0);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[1]->head->value)->key ==
+           key3);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[1]->head->value)->value ==
+           value3);
+
+    int *overlap_key = malloc(sizeof(int));
+    *overlap_key = 0;
+    int *overlap_value = malloc(sizeof(int));
+    *overlap_value = 40;
+
+    assert(hm_set(hashmap, overlap_key, overlap_value) == 0);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->value)->key ==
+           overlap_key);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->value)->value ==
+           overlap_value);
+    assert(
+        ((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->next->value)->key ==
+        key2);
+    assert(((struct HashMapLinkedListNode *)hashmap->buckets[0]->head->next->value)
+               ->value == value2);
+
+    hm_free(hashmap);
+
+    printf("hm_set passed\n");
+
+    return;
+}
+
 int main()
 {
     printf("Running tests for \"lib/hashmap.c\"\n");
 
     test_hm_create();
+    test_hm_set();
 
-    printf("All tests passed for \"lib/hashmap.c\"\n\n\n");
+    printf("All tests passed for \"lib/hashmap.c\"\n\n");
 
     return 0;
 }
